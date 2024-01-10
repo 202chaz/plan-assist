38c1b2563c1cde7d7d81287dceb3fd4e39cb1121
Chaz Haskins <chazhaskins@Chazs-MacBook-Pro.local>
2024-01-10 08:37:54 -0500

    fix logo

--- ui/src/components/Navbar.js
+++ ui/src/components/Navbar.js
@@ -9,7 +9,7 @@ export default function NavBar() {
     <Navbar expand="lg" className="bg-body-tertiary pt-3 pb-3 hbx-nav">
       <Container fluid>
         <Navbar.Brand href="#home">
-          <Image src="https://www.dchealthlink.com/themes/dchl/img/logo.png" />
+          <Image src="logo.png" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
